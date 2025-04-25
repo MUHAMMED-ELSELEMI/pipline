fe79971723100afcc1945f66911a0511247068fd
muhammed <muhammed.elselemi@ogr.sakarya.edu.tr>
2025-04-25 18:06:49 +0300

    first commit

---
+++ src/main/java/com/sau/testpipline/TestpiplineApplication.java
@@ -0,0 +1,13 @@
+package com.sau.testpipline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpiplineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestpiplineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sau/testpipline/TestpiplineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sau.testpipline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestpiplineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
